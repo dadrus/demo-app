8874560c2f0cb8af8148b085d62b619539c8bd14
Dimitrij Drus <dimitrij.drus@innoq.com>
2020-07-10 14:00:42 +0200

    added support for tls and making use of the gin-authn on github

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	authn "github.com/dadrus/gin-authn"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 var router *gin.Engine
@@ -15,5 +17,42 @@ func main() {
 
 	initRoutes()
 
-	router.Run(":8082")
+	var port string
+	if port = os.Getenv("PORT"); len(port) == 0 {
+		port = "8082"
+	}
+
+	if tlsConfig, err := getTlsConfig(); err == nil {
+		router.RunTLS(":" + port, tlsConfig.TlsCertFile, tlsConfig.TlsKeyFile)
+	} else {
+		router.Run(":" + port)
+	}
+}
+
+type tlsConfig struct {
+	TlsKeyFile  string
+	TlsCertFile string
+}
+
+func getTlsConfig() (*tlsConfig, error) {
+	tlsKeyFile := os.Getenv("TLS_KEY")
+	if len(tlsKeyFile) == 0 {
+		return nil, errors.New("no TLS key configured")
+	}
+	if _, err := os.Stat(tlsKeyFile); err != nil {
+		return nil, errors.New("configured TLS key not available")
+	}
+
+	tlsCertFile := os.Getenv("TLS_CERT")
+	if len(tlsCertFile) == 0 {
+		return nil, errors.New("no TLS cert configured")
+	}
+	if _, err := os.Stat(tlsCertFile); err != nil {
+		return nil, errors.New("configured TLS cert not available")
+	}
+
+	return &tlsConfig{
+		TlsKeyFile:  tlsKeyFile,
+		TlsCertFile: tlsCertFile,
+	}, nil
 }
